5c1a9b52244fdd56c0b57e39f5cefccbcdee13f9
Jean-Paul Weijers <18416852+jpweijers@users.noreply.github.com>
2022-12-07 05:13:45 +0000

    format

--- day06/solution.py
+++ day06/solution.py
@@ -5,10 +5,12 @@ def parse_input(file_name: str) -> str:
             signal += line
     return signal
 
+
 def find_unique_sequence(signal: str, length: int) -> int:
-    for i in range(len(signal[:-length-1])):
-        if len(set(signal[i:i+length])) == length:
+    for i in range(len(signal[: -length - 1])):
+        if len(set(signal[i : i + length])) == length:
             return i + length
+
 
 if __name__ == "__main__":
     signal = parse_input("input.txt")
