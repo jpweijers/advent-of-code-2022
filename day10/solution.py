9388eaead6ad8f0b408c96a6c721e72154bc96d2
Jean-Paul Weijers <18416852+jpweijers@users.noreply.github.com>
2022-12-10 10:42:16 +0000

    refactor day 10

--- day10/solution.py
+++ day10/solution.py
@@ -1,53 +1,52 @@
+from dataclasses import dataclass, field
+from typing import List
+
+
 def parse_input():
     with open("input.txt") as f:
         return [line.strip() for line in f]
 
 
 def part_one(lines):
-    x = 1
-    value = 0
-    measure_at = [20, 60, 100, 140, 180, 220]
-    cycles = 1
-    addx = 0
-    i = 0
-    action = lines[0].split()[0]
-    while cycles <= 221:
+    def add_to_result(x, cycles):
+        nonlocal result
         if cycles in measure_at:
-            print(f"cycles: {cycles}, x: {x}")
-            value += x * cycles
+            result += x * cycles
+
+    result = 0
+    x = 1
+    measure_at = {20, 60, 100, 140, 180, 220}
+    cycles = 0
+
+    for line in lines:
+        action = line.split()[0]
         if action == "noop":
-            i += 1
-            action = lines[i].split()[0]
-            if action == "addx":
-                addx = 0
-        elif action == "addx" and addx < 1:
-            addx += 1
-        else:
-            x += int(lines[i].split()[1])
-            i += 1
-            action = lines[i].split()[0]
-            if action == "addx":
-                addx = 0
-        cycles += 1
-    return value
+            cycles += 1
+            add_to_result(x, cycles)
+        if action == "addx":
+            cycles += 1
+            add_to_result(x, cycles)
+            cycles += 1
+            add_to_result(x, cycles)
+            x += int(line.split()[1])
 
-
-class Crt:
-    def __init__(self):
-        self.x = 1
-        self.screen = ["."] * 240
-
-    def write(self, cycles):
-        print(f"cycle: {cycles}, x: {self.x}, diff: {abs(self.x - cycles-1 % 40)})")
-        self.screen[cycles - 1] = "#" if abs(self.x - (cycles - 1) % 40) <= 1 else " "
-
-    def render(self):
-        return "".join(
-            "".join(self.screen[i * 40 : (i + 1) * 40]) + "\n" for i in range(6)
-        )
+    return result
 
 
 def part_two(lines):
+    @dataclass
+    class Crt:
+        x: int = 1
+        screen: List[str] = field(default_factory=lambda: [" "] * 240)
+
+        def write(self, cycles):
+            if abs(self.x - (cycles - 1) % 40) <= 1:
+                self.screen[cycles - 1] = "#"
+
+        def render(self):
+            screen = "".join(self.screen)
+            return "".join(screen[i * 40 : (i + 1) * 40] + "\n" for i in range(6))
+
     crt = Crt()
     cycles = 1
 
