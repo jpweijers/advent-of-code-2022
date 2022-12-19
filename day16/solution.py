9d5e971b7b51be7d173da4807f388f6e0713c916
Jean-Paul Weijers <18416852+jpweijers@users.noreply.github.com>
2022-12-19 19:26:49 +0000

    day 16 p1

--- day16/solution.py
+++ day16/solution.py
@@ -1,61 +1,79 @@
-import re
+from collections import defaultdict
+from itertools import product
 
 
-class Valve:
-    def __init__(self, name, flow_rate):
-        self.name = name
-        self.flow_rate = flow_rate
-        self.neighbors = []
+def parse_input(file_name):
+    rates = {}
+    graph = {}
 
-    def __repr__(self):
-        return f"{self.name} ({self.flow_rate}) -> {self.neighbors}"
+    with open(file_name) as f:
+        for field in map(str.split, f):
+            name = field[1]
+            flow_rate = int(field[4][5:-1])
+            neighbors = list(map(lambda x: x.rstrip(","), field[9:]))
+
+            rates[name] = flow_rate
+            graph[name] = neighbors
+
+    return rates, graph
 
 
-def parse_input(filename="test_input.txt"):
-    valves = {}
-    with open(filename) as f:
-        for line in f:
-            name, flow_rate, neighbours = re.findall(
-                r"Valve\s([A-Z]{2}).*?rate=(\d+).*?valves*\s(.*)",
-                line.strip(),
-            )[0]
-            valve = Valve(name, int(flow_rate))
-            valves[valve.name] = valve
-            for neighbour in neighbours.split(", "):
-                valve.neighbors.append(neighbour)
-           
+def score(rates, chosen_values):
+    return sum(rates[v] * time_left for v, time_left in chosen_values.items())
 
-    return valves
+
+def floyd_warshall(graph):
+    distances = defaultdict(lambda: defaultdict(lambda: float("inf")))
+
+    for a, bs in graph.items():
+        distances[a][a] = 0
+
+        for b in bs:
+            distances[a][b] = 1
+            distances[b][b] = 0
+
+    for a, b, c in product(graph, graph, graph):
+        bc, ba, ac = distances[b][c], distances[b][a], distances[a][c]
+
+        if ba + ac < bc:
+            distances[b][c] = ba + ac
+
+    return distances
+
+
+def dfs(distances, rates, valves, time=30, cur="AA", chosen: dict = {}):
+
+    for nxt in valves:
+        new_time = time - (distances[cur][nxt] + 1)
+        if new_time < 2:
+            continue
+        new_chosen = chosen | {nxt: new_time}
+        new_valves = valves - {nxt}
+        yield from dfs(distances, rates, new_valves, new_time, nxt, new_chosen)
+
+    yield chosen
+
 
 def part_one():
-    valves = parse_input()
-    position = valves["AA"]
-    total_flow = 0
-    flow_per_minute = 0
+    rates, graph = parse_input("input.txt")
+    distances = floyd_warshall(graph)
+    valves = set(graph.keys())
 
-    for _ in range(30, 0, -1):
-        print(f"pos: {position.name}, flow: {flow_per_minute}, total: {total_flow}")
-        input(valves)
-        if position.flow_rate > 0:
-            flow_per_minute += position.flow_rate
-            position.flow_rate = 0
+    result = 0
+    non_zero_valves = set(filter(rates.get, graph.keys()))
 
-        # now move
-        else:
-            _next = valves[position.neighbors[0]]
-            if len(position.neighbors) > 1:
-                for neighbour in position.neighbors:
-                    neighbour = valves[neighbour]
-                    if neighbour.flow_rate > _next.flow_rate:
-                        _next = neighbour
-                
+    for choice in dfs(distances, rates, non_zero_valves):
+        result = max(result, score(rates, choice))
 
-            position = _next
+    return result
 
-        total_flow += flow_per_minute
 
-    return total_flow
+def part_two():
+    rates, graph = parse_input("input.txt")
+
+    return None
 
 
 if __name__ == "__main__":
     print(f"Part one: {part_one()}")
+    print(f"Part two: {part_two()}")
